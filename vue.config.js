21f1d391e47e67080b9741e4df3ad928eb83db9f
hejunbin <hejunbin@aliyun.com>
2020-03-02 17:47:36 +0800

    测试自动更新

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                publish: [{
+                    "provider": "generic",
+                    "url": "https://checkstand.oss-cn-hangzhou.aliyuncs.com/download/"
+                }]
+            }
+        }
+    }
+}
